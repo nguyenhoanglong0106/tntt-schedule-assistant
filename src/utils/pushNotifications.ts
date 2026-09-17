0f97dcec2cd37bf47ddeae4db1d16b8bde6abd92
nguyenhoanglong0106 <messi162000bt@gmail.com>
2026-09-17 11:34:49 +0700

    Ok chốt

--- src/utils/pushNotifications.ts
+++ src/utils/pushNotifications.ts
@@ -9,7 +9,9 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const raw = atob(base64)
-  return Uint8Array.from(raw, c => c.charCodeAt(0))
+  const array = new Uint8Array(raw.length)
+  for (let i = 0; i < raw.length; i++) array[i] = raw.charCodeAt(i)
+  return array
 }
 
 export async function registerPushSubscription(): Promise<boolean> {
